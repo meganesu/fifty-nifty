8bfa2efe9bde483f43b0f58fa25ac4a0970a7ae6
Megan Sullivan <meganesulli@gmail.com>
2019-10-30 19:48:07 -0700

    feat: Add initial UnitedStatesMapSVG component; move components into subfolders

---
+++ src/components/UnitedStatesMapSVG/UnitedStatesMapSVG.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UnitedStatesMapSVG = () => (
+  <svg
+    aria-label="A map of the United States, showing which states I have visited."
+  >
+  </svg>
+);
+
+export default UnitedStatesMapSVG;
