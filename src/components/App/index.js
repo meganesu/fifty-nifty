92123d52d6083a98f896ca726f945bcb59ff9317
Megan Sullivan <msulliv@thoughtworks.com>
2019-10-31 22:51:54 -0700

    feat: pass statesVisited prop into UnitedStatesMapSVG in App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import UnitedStatesMapSVG from '../UnitedStatesMapSVG';
+import statesVisited from './statesVisited';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fifty Nifty</h1>
-      <UnitedStatesMapSVG />
+      <UnitedStatesMapSVG statesVisited={statesVisited} />
       <p id="source-link">View source on <a href="https://github.com/meganesu/fifty-nifty/">GitHub</a>.</p>
     </div>
   );
--- src/components/App/index.spec.js
+++ src/components/App/index.spec.js
@@ -9,14 +9,18 @@ describe('App', () => {
   beforeAll(() => {
     component = mount(<App />);
   });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('renders a UnitedStatesMapSVG', () => {
-    expect(component.find(UnitedStatesMapSVG)).toHaveLength(1);
+  it('renders a UnitedStatesMapSVG with the correct props', () => {
+    const svg = component.find(UnitedStatesMapSVG);
+    expect(svg).toHaveLength(1);
+    expect(svg.prop('statesVisited')).toBeDefined();
+    expect(Array.isArray(svg.prop('statesVisited'))).toBe(true);
   });
 
   it('renders a heading', () => {
