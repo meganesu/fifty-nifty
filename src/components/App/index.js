7615489c43a0b4bfa61c99f4ec4a79e02fa3acd6
Megan Sullivan <msulliv@thoughtworks.com>
2019-10-31 23:37:43 -0700

    feat: add description of page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,8 @@ function App() {
   return (
     <div className="App">
       <h1>Fifty Nifty</h1>
+      <p>One of the things on my bucket list is to visit all 50 states in the U.S.</p>
+      <p>I've started many physical lists to keep track of which states I've been to so far, but I keep forgetting where I put them, so I've decided to use the Internet to keep track instead!</p>
       <UnitedStatesMapSVG statesVisited={statesVisited} />
       <p id="source-link">View source on <a href="https://github.com/meganesu/fifty-nifty/">GitHub</a>.</p>
     </div>
